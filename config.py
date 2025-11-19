0043a2e2f8f577c6a6710076ea6d5f4a857be8df
yuuri1234yu-droid <yuuri1234yu@gmail.com>
2025-11-19 16:47:08 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-import os
+# config.py
 
-TELEGRAM_BOT_TOKEN = "8455133544:AAE_aaQuzWkxgfR4xSTiwJBo8Wf6CXykyeg"
-TELEGRAM_CHAT_ID = "5917411414"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "")
